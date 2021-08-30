362198f389677e2e6f714373b8f61951e522f8e0
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-08-30 15:02:13 -0300

    Create exerc28.py

---
+++ exerc28.py
@@ -0,0 +1,9 @@
+num =int(input("Digite um valor :"))#receber valor para calculo
+num2 =int(input("Digite outro valor :"))#receber valor para calculo
+num3 =int(input("Digite mais um valor :"))#receber valor para calculo
+Q1 = num*num#calcular o quadrado de cada numero
+Q2 = num2*num2#calcular o quadrado de cada numero
+Q3 = num3*num3#calcular o quadrado de cada numero
+S = Q1 + Q2 + Q3#calcular soma dos quadrados
+
+print(S)#apresentar o calculo S
