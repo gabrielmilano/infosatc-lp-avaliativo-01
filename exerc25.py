19a305bc6f43d41b77988017930e8b52f2f46779
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-08-30 15:01:40 -0300

    Create exerc25.py

---
+++ exerc25.py
@@ -0,0 +1,4 @@
+num =int(input("Digite um valor de area em acres para converter para metros quadrados :"))#receber valor para calculo
+M = num*4048.58#acres para metros quadrados
+print(M)#mostrar resultado
+
