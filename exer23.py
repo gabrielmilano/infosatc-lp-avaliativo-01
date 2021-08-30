4ea59460bf0707b7a47490de5db903927231c083
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-08-30 15:01:36 -0300

    Create exer23.py

---
+++ exer23.py
@@ -0,0 +1,4 @@
+num =int(input("Digite um valor em metros para converter para jardas :"))#receber valor em metros
+J = num/0.91#calcular para jardas
+print(J)#mostrar resultado
+print("Jardas")#mostrar resultado
