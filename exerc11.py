454a01673c7027607a8afc206c8db61f340bab18
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-08-30 15:01:18 -0300

    Create exerc11.py

---
+++ exerc11.py
@@ -0,0 +1,4 @@
+num =int(input("Digite um valor de metros por segundo para converter em KM/h :"))#receber valor
+K = num * 3.6#calcular de metros por segundo para KM/h
+print(K) #mostrar resultado
+print("Km/h")
