caf080ece2ad238723b5a5da792023213e556733
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-08-30 15:01:43 -0300

    Create exerc26.py

---
+++ exerc26.py
@@ -0,0 +1,4 @@
+num =int(input("Digite um valor em metros quadrados para converter em hectares :"))#receber valor para calculo
+H = num*0.0001#calcular conversao em hectares
+print(H)#mostrar resultado
+
